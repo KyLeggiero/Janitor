715631cb59043dc4b0b4d342e8b758750e25096c
Ben Leggiero <BenLeggiero@Gmail.com>
2020-09-08 20:48:06 -0600

    WIP: Pulling bunches of stuff from the other MVP branch

---
+++ Janitor/Shared/Constants/Color Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Color Constants.swift
+//  Janitor
+//
+//  Created by Ben Leggiero on 7/19/19.
+//  Copyright © 2019 Ben Leggiero. All rights reserved.
+//
+
+import SwiftUI
+
+
+
+extension Color {
+    static let watchedFolderSettingsBackground = Color("Watched Folder Settings Background")
+    static let watchedFolderSettingsBorder = Color("Watched Folder Settings Border")
+}
