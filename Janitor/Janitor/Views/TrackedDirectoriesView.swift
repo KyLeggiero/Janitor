62ab40cb2cd6c6f4e8b4992821b521b0174d9177
Ben Leggiero <BenLeggiero@Gmail.com>
2021-07-19 00:26:01 -0600

    Starting to rebuild UI from scratch now that SwiftUI is more mature

--- Janitor/Janitor/Views/TrackedDirectoriesView.swift
+++ Janitor/Janitor/Views/TrackedDirectoriesView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 import JanitorKit
+import SimpleLogging
 
 
 
@@ -16,6 +17,12 @@ struct TrackedDirectoriesView: View {
     @Binding
     var trackedDirectories: [TrackedDirectory]
     
+    @State
+    private var isSelectingNewDirectoryToTrack = false
+    
+    @State
+    private var nextTrackedDirectory: TrackedDirectory? = nil
+    
     
     init(_ trackedDirectories: Binding<[TrackedDirectory]>) {
         self._trackedDirectories = trackedDirectories
@@ -23,10 +30,52 @@ struct TrackedDirectoriesView: View {
     
     
     var body: some View {
-        List(trackedDirectories) { dir in
-            Text(dir.url.absoluteString)
+        VStack(alignment: .leading) {
+            List($trackedDirectories) { dir in
+                TrackedDirectoryView(dir)
+            }
+            .listStyle(InsetListStyle())
+            
+            Divider()
+            
+            Spacer(minLength: 0).fixedSize()
+            
+            HStack {
+                Button(action: selectNewDirectoryToTrack) {
+//                    HStack {
+                        Image(systemName: "plus")
+                        Text("Track another")
+//                    }
+                }
+                .padding()
+            }
+            .controlSize(.large)
+//            .buttonStyle(DefaultButtonStyle())
         }
-        .listStyle(InsetListStyle())
+        .fileImporter(isPresented: $isSelectingNewDirectoryToTrack,
+                      allowedContentTypes: [.directory]) { result in
+            switch result {
+            case .success(let directoryUrl):
+                nextTrackedDirectory = .init(
+                    uuid: UUID(),
+                    isEnabled: true,
+                    url: directoryUrl,
+                    oldestAllowedAge: .init(value: 60, unit: .day),
+                    largestAllowedTotalSize: .init(value: 5, unit: .gibibyte))
+                
+            case .failure(let error):
+                log(error: error)
+                assertionFailure()
+            }
+        }
+    }
+}
+
+
+
+private extension TrackedDirectoriesView {
+    func selectNewDirectoryToTrack() {
+        
     }
 }
 
@@ -37,9 +86,14 @@ struct TrackedDirectoriesView_Previews: PreviewProvider {
         TrackedDirectoriesView(.constant([
             .init(uuid: UUID(),
                   isEnabled: true,
-                  url: URL(fileURLWithPath: "/Path/To/File"),
+                  url: URL(fileURLWithPath: "/Path/To/File1"),
                   oldestAllowedAge: Age(value: 30, unit: .day),
-                  largestAllowedTotalSize: DataSize(value: 1, unit: .gibibyte))
+                  largestAllowedTotalSize: DataSize(value: 1, unit: .gibibyte)),
+            .init(uuid: UUID(),
+                  isEnabled: false,
+                  url: URL(fileURLWithPath: "/Path/To/File2"),
+                  oldestAllowedAge: Age(value: 30, unit: .day),
+                  largestAllowedTotalSize: DataSize(value: 1, unit: .gibibyte)),
         ]))
     }
 }
---
+++ Janitor/Janitor/Views/TrackedDirectoryView.swift
@@ -0,0 +1,38 @@
+//
+//  TrackedDirectoryView.swift
+//  TrackedDirectoryView
+//
+//  Created by Ben Leggiero on 2021-07-18.
+//
+
+import SwiftUI
+import JanitorKit
+
+
+
+struct TrackedDirectoryView: View {
+    
+    @Binding
+    var trackedDirectory: TrackedDirectory
+    
+    
+    init(_ trackedDirectory: Binding<TrackedDirectory>) {
+        self._trackedDirectory = trackedDirectory
+    }
+    
+    
+    var body: some View {
+        Text("Tracking \(trackedDirectory.url)")
+    }
+}
+
+struct TrackedDirectoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        TrackedDirectoryView(.constant(.init(
+            uuid: UUID(),
+            isEnabled: true,
+            url: URL(fileURLWithPath: "/Path/To/File.txt"),
+            oldestAllowedAge: .init(value: 7, unit: .day),
+            largestAllowedTotalSize: .init(value: 2, unit: .gibibyte))))
+    }
+}
